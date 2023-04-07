a29a9d5565862b39b008c36c0b4e96fac87f85dc
Punamkumari03 <punamgupta250602@gmail.com>
2023-04-08 00:55:24 +0530

    ashboard
    Assignments
    
    Leaderboard
    
    Forum
    
    Interview
    
    Team
    
    Description
    Hints
    Styling and using JS in JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,18 @@
+import "./ExpenseItem.css"
 function ExpenseItem(){
-    return (<div>
-    <h2>Expense item!</h2>
-    <div>Food Rs 10</div>
-    <div>Petrol Rs 100</div>
-    <div>Movies Rs 200</div>
-   
-
-    </div>
+    const expenseDate=new Date(2021,2,28);
+    const expenseTitle='Car Insurance';
+    const expenseAmount=294.67;
+    const LocationOfExpenditure='patna';
+    return (
+        <div className="expense-item">
+        <div>{expenseDate.toISOString()}</div>
+        <div className="expense-item__description">
+          <h2>{expenseTitle}</h2>
+          <div className="expense-item__price">${expenseAmount}</div>
+          <div>{LocationOfExpenditure}</div>
+        </div>
+      </div> 
     );
 }
 export default ExpenseItem;
